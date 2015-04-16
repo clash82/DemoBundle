874e17228a472c5152dc2d1379950c31597e83ae
Rafał Toborek <rafal.toborek@ez.no>
2015-04-16 15:16:28 +0200

    EZP-24231: As a Developer I would like to have more abstract structure of the PrivacyCookieBundle
    Link to Jira task: https://jira.ez.no/browse/EZP-24231

---
+++ DependencyInjection/eZDemoCompilerPass.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace EzSystems\DemoBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\Reference;
+
+class eZDemoCompilerPass implements CompilerPassInterface
+{
+    protected $container;
+
+    private function addPrivacyCookieBanner()
+    {
+        if ($this->container->hasDefinition('ez_privacy_cookie.twig.extension') === false) {
+            return;
+        }
+
+        if ($this->container->hasDefinition('ezdemo.ez_content_banner_factory') === false) {
+            return;
+        }
+
+        $definition = $this->container->getDefinition('ez_privacy_cookie.twig.extension');
+        $eZBannerFactory = $this->container->getDefinition('ezdemo.ez_content_banner_factory');
+        $definition->replaceArgument(2, $eZBannerFactory);
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        $this->container = $container;
+
+        $this->addPrivacyCookieBanner();
+    }
+}
